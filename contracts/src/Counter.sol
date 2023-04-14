c570008e51951a8718a38390ca7b8ab8e2e03cb7
Locke <benkim96@gmail.com>
2023-04-15 00:03:37 +0900

    struct

--- contracts/src/Counter.sol
+++ contracts/src/Counter.sol
@@ -2,13 +2,21 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    struct proposal {
+        address borrower;
+        address lender;
+        uint256 amount;
+        address token;
+        // simple annualized interest in bps
+        uint256 interest;
+        uint256 loanLengthDays;
+        uint256 blockNumberEnd;
     }
+    // 1% interest (0.01) -> 100 bps
+    uint256 public bps_numberator = 10000;
+    //    1% -> 1 * 10^4 = 10,000  (interest in bps)
+    //      1 day of interest at a 1% annualized rate
+    //      1% / 365 -> 10,000 / 365 = 27
 
-    function increment() public {
-        number++;
-    }
+
 }
