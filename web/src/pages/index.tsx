92ee8b1c7dd3c2fae019cdfd48acff1fe02e38fc
Locke <benkim96@gmail.com>
2023-04-15 11:25:29 +0900

    fix build issue

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,9 +2,8 @@ import { useAccount, useBalance } from "wagmi";
 
 export default function Home() {
     const { address } = useAccount();
-    const { data } = useBalance(address);
+    // const { data } = useBalance(address);
 
-    console.log(data);
     return (
         <div className="flex flex-grow flex-col">
             <HeroSection />
