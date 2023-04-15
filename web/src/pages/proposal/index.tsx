4e8b2e2f1ec7afd0dd59cce8c5dfc164955f35f2
Locke <benkim96@gmail.com>
2023-04-15 14:39:02 +0900

    proposal screen with server side props

--- web/src/pages/proposal/index.tsx
+++ web/src/pages/proposal/index.tsx
@@ -1,3 +1,39 @@
-export default function Proposal() {
-    return <>My Proposals</>;
+import { ProposalType, ProposalsType } from "../../../common/types";
+
+export default function Proposal(props: ProposalsType) {
+    console.log(props);
+    return <div>{props.proposals[0].borrowerAddress}</div>;
+}
+
+export async function getServerSideProps() {
+    // const res = await fetch(`https://.../data`);
+
+    const props: ProposalsType = {
+        proposals: [
+            {
+                borrowerAddress: "",
+                tokenAddress: "",
+                principal: 0,
+                interest: 0,
+                lengthDays: 0,
+                lenderAddress: "",
+                fundDay: 0,
+                repaid: false,
+            },
+            {
+                borrowerAddress: "",
+                tokenAddress: "",
+                principal: 0,
+                interest: 0,
+                lengthDays: 0,
+                lenderAddress: "",
+                fundDay: 0,
+                repaid: false,
+            },
+        ],
+    };
+
+    return {
+        props: props,
+    };
 }
