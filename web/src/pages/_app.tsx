29e5946932e656865afaf10f4a3d4fa62600f319
spennyp <hello@spencerperkins.me>
2023-04-16 01:01:29 +0900

    add linea

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,13 +2,31 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { WagmiConfig, createClient } from "wagmi";
 import { ConnectKitProvider, ConnectKitButton, getDefaultClient, useIsMounted } from "connectkit";
-import { mainnet, polygon, optimism, arbitrum, localhost } from "wagmi/chains";
+import { polygon, localhost, gnosis, scrollTestnet } from "wagmi/chains";
 import "../globals.css";
+
+export const linea = {
+    id: 59140,
+    name: "Linea",
+    network: "Linea",
+    nativeCurrency: {
+        decimals: 18,
+        name: "LineaETH",
+        symbol: "LineaETH",
+    },
+    rpcUrls: {
+        public: { http: ["https://rpc.goerli.linea.build"] },
+        default: { http: ["https://rpc.goerli.linea.build"] },
+    },
+    blockExplorers: {
+        default: { name: "Linea", url: "https://explorer.goerli.linea.build/" },
+    },
+} as const satisfies Chain;
 
 import MainLayout from "@/layouts/MainLayout";
 
 const alchemyId = process.env.NEXT_PUBLIC_ALCHEMY_ID;
-const chains = [mainnet, polygon, optimism, arbitrum, localhost];
+const chains = [polygon, localhost, gnosis, scrollTestnet, linea];
 
 const client = createClient(
     getDefaultClient({
