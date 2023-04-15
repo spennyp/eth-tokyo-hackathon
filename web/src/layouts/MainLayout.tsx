275bdfae9c256c71a9399a10d0d3a7d912ff599f
Locke <benkim96@gmail.com>
2023-04-15 11:24:36 +0900

    update header for some styling

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -1,29 +1,55 @@
 import { Flex, Text, Stack, Box } from "@chakra-ui/layout";
 import { ConnectKitButton } from "connectkit";
-import { ReactNode } from "react";
-import NextLink from "next/link";
-import { Link } from "@chakra-ui/react";
+import { ReactNode, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export interface MainLayoutProps {
     children: ReactNode;
 }
 
 function Header() {
+    const headerTextButtonStyle =
+        "cursor-pointer hidden md:block text-[14px] font-semibold pt-1 text-linkDisabled underline-offset-4 hover:underline";
+    const activeHeaderTextButtonStyle =
+        "cursor-pointer hidden md:block text-[14px] font-semibold pt-1 underline-offset-4 text-primary underline";
+    const router = useRouter();
+    const [path, setPath] = useState("");
+
+    // determines which path or sub element we are focused on
+    useEffect(() => {
+        const pathParts = router.asPath.split("#");
+        if (pathParts.length >= 2) {
+            const hash = pathParts.slice(-1)[0];
+            setPath(hash);
+        } else {
+            setPath(router.asPath);
+        }
+    }, [router.asPath]);
+
     return (
         <div className="w-full flex flex-row justify-between items-center p-4 bg-secondaryBg">
             <div className="flex flex-row space-x-8">
                 <Text>LOGO</Text>
-                <Stack spacing={4} direction="row" align="center">
-                    <Link as={NextLink} href="/">
-                        Home
+                <div className="flex flex-row justify-start items-center space-x-8">
+                    <Link href="/">
+                        <p className={path === "/" ? activeHeaderTextButtonStyle : headerTextButtonStyle}>Home</p>
                     </Link>
-                    <Link as={NextLink} href="/explore">
-                        Explore
+                    <Link href="/explore">
+                        <p className={path === "/explore" ? activeHeaderTextButtonStyle : headerTextButtonStyle}>
+                            Explore
+                        </p>
                     </Link>
-                    <Link as={NextLink} href="/proposal/create">
-                        Create
+                    <Link href="/proposal/create">
+                        <p
+                            className={
+                                path === "/proposal/create" ? activeHeaderTextButtonStyle : headerTextButtonStyle
+                            }
+                        >
+                            Create
+                        </p>
                     </Link>
-                </Stack>
+                </div>
             </div>
             <div>
                 <ConnectKitButton showBalance={true} />
